c922d0a31d545ad2005616833cf98675f5d481c2
Yasmin Beaumont <96609891+VanellopeReal@users.noreply.github.com>
2022-03-12 02:37:41 -0300

    Add files via upload

---
+++ Pranks and Trolls/Create New Desktop/desktop_troll.py
@@ -0,0 +1,12 @@
+from pyautogui import hotkey
+from time import sleep
+# Code
+sleep(0.250)
+hotkey("win", "ctrl", "d")
+sleep(0.500)
+# If you Want to Get an Infinite Desktop Creator (please use sleep(5)), use:
+#key = True
+#while key == True:
+#   sleep(0.250)
+#   hotkey("win", "ctrl", "d")
+#   sleep(5)
